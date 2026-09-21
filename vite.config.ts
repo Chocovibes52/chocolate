383e94df4eca642445222697f06f6c7eec79bd1d
Chocovibes52 <Chocovibes52@gmail.com>
2026-09-21 12:43:23 +0530

    this added notro

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  tanstackStart: {
-    server: { entry: "server" },
-  },
-  nitro: {
-    preset: "vercel",
-  },
+  plugins: [
+    tailwindcss(),
+    viteTsConfigPaths(),
+    tanstackStart(),
+    nitro(),
+    viteReact(),
+  ],
+
   server: {
     host: "0.0.0.0",
     port: 3000,
