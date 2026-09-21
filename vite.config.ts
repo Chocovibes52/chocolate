a9f1a33db23d3e80d365c67026b2734f9b873528
Chocovibes52 <Chocovibes52@gmail.com>
2026-09-21 12:47:51 +0530

    Fix TanStack SSR renderer for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,34 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    viteTsConfigPaths(),
-    tanstackStart(),
-    nitro(),
-    viteReact(),
-  ],
-
   server: {
     host: "0.0.0.0",
     port: 3000,
     allowedHosts: true,
   },
+
+  resolve: {
+    tsconfigPaths: true,
+  },
+
+  plugins: [
+    tailwindcss(),
+
+    tanstackStart({
+      server: {
+        entry: "server",
+      },
+    }),
+
+    viteReact(),
+
+    nitro({
+      renderer: false,
+    }),
+  ],
 });
